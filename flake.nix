a39f6e058f2aee2aada7d77982abd3f0c60b8bf9
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-09-25 18:23:56 +0200

    build: add nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Go Kit Protoc Compiler";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildDeps = with pkgs; [ git go gnumake ];
+        devDeps = with pkgs;
+          buildDeps ++ [
+            golangci-lint
+            gotestsum
+            goreleaser
+            protobuf
+            protoc-gen-go
+            protoc-gen-go-grpc
+          ];
+      in { devShell = pkgs.mkShell { buildInputs = devDeps; }; });
+}
